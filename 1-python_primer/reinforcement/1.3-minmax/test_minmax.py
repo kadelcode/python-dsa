0ddffc6847e7e2093f4bbff228518235501295f7
Ayodele Kadiri <kadelcode@gmail.com>
2024-07-02 21:27:20 +0100

    Add more tests

--- 1-python_primer/reinforcement/1.3-minmax/test_minmax.py
+++ 1-python_primer/reinforcement/1.3-minmax/test_minmax.py
@@ -26,5 +26,19 @@ class TestMinmax(unittest.TestCase):
         """
         self.assertEqual(minmax([5]), (5,5))
 
+    def test_sorted_list(self):
+        """
+        Tests if the function correctly finds min and
+        max for a sorted list
+        """
+        self.assertEqual(minmax([1, 2, 3, 4, 5]), (1, 5))
+
+    def test_unsorted_list(self):
+        """
+        Tests if the function correctly finds the min and
+        max for an unsorted list
+        """
+        self.assertEqual(minmax([6, 5, 20, 3, 7]), (3, 20))
+
 if __name__ == '__main__':
     unittest.main()
