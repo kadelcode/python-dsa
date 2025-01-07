7aa1ecb131a8fd2ecea88fa4c94a352ee3290cc7
kadelcode <kadelcode@gmail.com>
2025-01-07 23:21:25 +0100

    Test for special cases

--- 1-python_primer/creativity/1.13-reverse_list_integers/test_reverse_list.py
+++ 1-python_primer/creativity/1.13-reverse_list_integers/test_reverse_list.py
@@ -12,6 +12,12 @@ class TestReverseList(unittest.TestCase):
     def test_single_element(self):
         self.assertEqual(reverse_list([20]), [20])
 
+    def test_negative_numbers(self):
+        self.assertEqual(reverse_list([-1, -2, -3, -4]), [-4, -3, -2, -1])
+
+    def test_duplicates(self):
+        self.assertEqual(reverse_list([1, 1, 2, 2, 3, 3]), [3, 3, 2, 2, 1, 1])
+
 
 if __name__ == "__main__":
     unittest.main()
