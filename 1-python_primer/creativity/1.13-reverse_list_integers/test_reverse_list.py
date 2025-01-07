5c5f0a0a9711ba223a8cd8e6a64936ea9805b2e5
kadelcode <kadelcode@gmail.com>
2025-01-07 20:24:59 +0100

    Edge cases

--- 1-python_primer/creativity/1.13-reverse_list_integers/test_reverse_list.py
+++ 1-python_primer/creativity/1.13-reverse_list_integers/test_reverse_list.py
@@ -6,6 +6,12 @@ class TestReverseList(unittest.TestCase):
     def test_basic(self):
         self.assertEqual(reverse_list([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
 
+    def test_empty(self):
+        self.assertEqual(reverse_list([]), [])
+
+    def test_single_element(self):
+        self.assertEqual(reverse_list([20]), [20])
+
 
 if __name__ == "__main__":
     unittest.main()
