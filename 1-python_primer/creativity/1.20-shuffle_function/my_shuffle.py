6ce74d346f927ac91b06458b12e9fa8edd7cf7df
kadelcode <kadelcode@gmail.com>
2025-01-22 21:33:04 +0100

    Shuffle function

--- 1-python_primer/creativity/1.20-shuffle_function/my_shuffle.py
+++ 1-python_primer/creativity/1.20-shuffle_function/my_shuffle.py
@@ -1,2 +1,15 @@
+import random
+
 def my_shuffle(data):
-    pass
+    """
+    Shuffle the elements of a list in place using the Fisher-Yates Shuffle algorithm
+    and the randint function from the random module
+    """
+    for i in range(len(data) - 1, 0, -1):
+        # Generate a random index from 0 to i (inclusive)
+        j = random.randint(0, i)
+
+        # Swap index of the current element with the randomly selected one
+        data[i], data[j] = data[j], data[i]
+
+    return data
