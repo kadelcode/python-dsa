8226e90765a764391d74910d639e3001e19c3e7f
kadelcode <kadelcode@gmail.com>
2025-01-26 16:13:47 +0100

    Add more tests

--- 1-python_primer/creativity/1.24-number_of_vowels/test_count_vowels.py
+++ 1-python_primer/creativity/1.24-number_of_vowels/test_count_vowels.py
@@ -12,5 +12,14 @@ class TestCountVowels(unittest.TestCase):
     def test_all_vowels_lowercase(self):
         self.assertEqual(count_vowels("aeiou"), 5)
 
+    def test_all_vowels_uppercase(self):
+        self.assertEqual(count_vowels("AEIOU"), 5)
+
+    def test_mixed_case(self):
+        self.assertEqual(count_vowels("Hello World"), 3)
+
+    def test_with_numbers_and_symbols(self):
+        self.assertEqual(count_vowels("123!@#aBcDe"), 2)
+
 if __name__ == "__main__":
     unittest.main()
