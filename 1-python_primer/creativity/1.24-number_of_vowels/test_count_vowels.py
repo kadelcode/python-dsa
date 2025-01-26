8fad2aa4e9c54c31d0ac3ccb7c5a34dcca61476a
kadelcode <kadelcode@gmail.com>
2025-01-26 13:13:45 +0100

    New test cases

--- 1-python_primer/creativity/1.24-number_of_vowels/test_count_vowels.py
+++ 1-python_primer/creativity/1.24-number_of_vowels/test_count_vowels.py
@@ -2,7 +2,15 @@ import unittest
 from count_vowels import count_vowels
 
 class TestCountVowels(unittest.TestCase):
-    pass
+    
+    def test_empty_string(self):
+        self.assertEqual(count_vowels(""), 0)
+
+    def test_no_vowels(self):
+        self.assertEqual(count_vowels("bcdfgh"), 0)
+
+    def test_all_vowels_lowercase(self):
+        self.assertEqual(count_vowels("aeiou"), 5)
 
 if __name__ == "__main__":
     unittest.main()
