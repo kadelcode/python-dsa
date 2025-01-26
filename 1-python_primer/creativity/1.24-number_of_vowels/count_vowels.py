2b1230c5071838314ce83ea3fbeddb3d9aa378e3
kadelcode <kadelcode@gmail.com>
2025-01-26 15:58:47 +0100

    Code logic

--- 1-python_primer/creativity/1.24-number_of_vowels/count_vowels.py
+++ 1-python_primer/creativity/1.24-number_of_vowels/count_vowels.py
@@ -1,2 +1,15 @@
 def count_vowels(input_string):
-    pass
+    """Counts the number of vowels (a, e, i, o, u) in a string.
+
+    Args:
+        input_string: The string to check.
+    Returns:
+        The number of vowels in the string.
+    """
+    vowels = "aeiouAEIOU"
+
+    count = 0
+    for char in input_string:
+        if char in vowels:
+            count += 1
+    return count
